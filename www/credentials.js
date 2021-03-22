f22ec3886e0290c46a970d702425930f835e60ad
Marcelo <mabc@live.cl>
2021-03-22 19:53:47 -0300

    Se creó el proyecto tv-shows, sólo lista un documento de Firebase

---
+++ www/credentials.js
@@ -0,0 +1,29 @@
+
+export const credentials = {
+    apiKey:"AIzaSyCSMalZ_mVWs3z2LcYvmN6nX5vvgsiyORc",
+    authDomain:"hola-app-5078c.firebaseapp.com",
+    databaseURL:"https://hola-app-5078c.firebaseio.com",
+    projectId:"hola-app-5078c",
+    storageBucket:"hola-app-5078c.appspot.com",        // because we don't have a database yet, this will be an empty string for now
+    messageSenderId:"418173894390",
+    appId:"1:418173894390:web:e7d92a693a6066affd76d7",
+    measurementId: "G-8S19KZG2W0",
+}
+
+
+/*
+var firebaseConfig = {
+    apiKey: "AIzaSyCSMalZ_mVWs3z2LcYvmN6nX5vvgsiyORc",
+    authDomain: "hola-app-5078c.firebaseapp.com",
+    databaseURL: "https://hola-app-5078c.firebaseio.com",
+    projectId: "hola-app-5078c",
+    storageBucket: "hola-app-5078c.appspot.com",
+    messagingSenderId: "418173894390",
+    appId: "1:418173894390:web:e7d92a693a6066affd76d7",
+    measurementId: "G-8S19KZG2W0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+*/  
+
+  
